36626880fcc78ae5c25e3757246355e4f0041025
58dh <67215039+58dh@users.noreply.github.com>
2020-06-24 20:26:01 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+//微信自动跳转源码(市面上最好最全面的防封防屏蔽源码)
+//最后更新：2019/10/15
+//开发者：唯一作坊
+//免责声明：本源码仅供学习交流研究之用，请勿用于商业途径及非法运营，购买用户严禁将本店所有产品用于与中国以及亚洲范围内所有的（包含台湾、澳门、香港等在内的省、独立区、特区、岛国）国家的现行法律相违背的一切行为；否则，请停止购买本源码，如坚持使用一切法律责任 及 所有后果均由使用方承担！与本店无关！特此声明。
+error_reporting(0);
+header('Content-Type: text/html; charset=utf-8');
+header('Cache-Control: no-store, no-cache');
+$geturl = 'http://58.bbyos.cn/jkfv4y';
+$shebeiid = get_device_type();
+if(strpos($_SERVER['HTTP_USER_AGENT'], 'QQ/')!==false){
+if($shebeiid == 'android'){
+echo '<!DOCTYPE html>
+<html>
+ <head>
+  <title>正在打开浏览器...</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	  
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">	
+	<style>
+		body{background-color:#333;}
+		img{width:100%;}
+	</style>  
+ </head>
+ <body><img src="https://img.alicdn.com/imgextra/i4/668603298/TB2ZuSVgQKWBuNjy1zjXXcOypXa_!!2-martrix_bbs.png" /></body>
+</html>';
+exit;	
+}else{
+echo '<!DOCTYPE html>
+<html>
+<head>
+    <title>正在打开浏览器...</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">	
+	<style>
+		body{background-color:#333;}
+		img{width:100%;}
+	</style>
+</head>
+<body>
+<img src="https://img.alicdn.com/imgextra/i4/741840578/TB2qdJlpWAoBKNjSZSyXXaHAVXa_!!2-martrix_bbs.png" />
+</body>
+</html>';
+exit;	
+}
+}elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger')!==false){
+if($shebeiid == 'android'){
+header('Content-Disposition: attachment; filename="30006804102.pdf"');
+header('Content-Type: application/uomg');
+exit;	 
+}else{
+echo '<!DOCTYPE html>
+<html>
+<head>
+    <title>正在打开浏览器...</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">	
+	<style>
+		body{background-color:#333;}
+		img{width:100%;}
+	</style>
+</head>
+<body>
+<img src="https://img.alicdn.com/imgextra/i4/741840578/TB2qdJlpWAoBKNjSZSyXXaHAVXa_!!2-martrix_bbs.png" />
+<!--风格1<img src="https://img.alicdn.com/imgextra/i4/741840578/TB2qdJlpWAoBKNjSZSyXXaHAVXa_!!2-martrix_bbs.png" />-->
+<!--风格2<img src="https://img.alicdn.com/imgextra/i4/668603298/TB2ZuSVgQKWBuNjy1zjXXcOypXa_!!2-martrix_bbs.png" />
+根据自己选择;正式使用请把这些注释删除即可-->
+</body>
+</html>';
+exit;
+}
+}else{
+	exit('<script>window.location.href="'.$geturl.'";</script>');
+}
+function get_device_type()
+{
+ $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+ $type ='other';
+ if(strpos($agent,'iphone') || strpos($agent,'ipad'))
+ {
+ $type ='ios';
+ }
+ if(strpos($agent,'android'))
+ {
+ $type ='android';
+ }
+ return $type;
+}
+?>
